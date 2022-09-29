298462d924e914edf36cfb781a327da0ad40b116
Matheus <matheusosp>
2022-09-29 20:55:18 -0300

    implementando TODO'S

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia(numero: "9945158587", modelo: "5", IMEI: "6", memoria: 12);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone(numero: "47545463", modelo: "4555", IMEI: "454545", memoria: 32);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
